7bdf4773992d575e459f7c7ba1fffce57ab58ffd
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2024-09-03 10:42:35 -0500

    Add server status dynamic logic

--- 20 - v17-components-templates/src/app/server-status/server-status.component.ts
+++ 20 - v17-components-templates/src/app/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      const rdm = Math.random();
+
+      if (rdm < 0.5) {
+        this.currentStatus = 'offline';
+      } else if (rdm < 0.9) {
+        this.currentStatus = 'unknown';
+      } else {
+        this.currentStatus = 'online';
+      }
+    }, 5000);
+  }
 }
--- 20 - v17-components-templates/src/app/shared/control/control.component.ts
+++ 20 - v17-components-templates/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,8 +6,29 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
+  // @HostBinding('class') classValue = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
+
   label = input.required<string>();
+  hostElement = inject(ElementRef);
+  // hostElement!: ElementRef;
+
+  // constructor(_hostElement: ElementRef) {
+  //   this.hostElement = _hostElement;
+  // }
+
+  // constructor(private hostElement: ElementRef) { }
+
+  onClick() {
+    console.log(this.hostElement);    
+  }
 }
